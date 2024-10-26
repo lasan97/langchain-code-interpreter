449789870551c9b39ecbdbce4f3d36d7c072beeb
Martin <martin@niceday.io>
2024-10-26 20:23:16 +0900

    implement route agent

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from typing import Any
+
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import create_react_agent, AgentExecutor, AgentType
+from langchain_core.tools import Tool
 from langchain_experimental.agents import create_csv_agent
 from langchain_experimental.tools import PythonREPLTool
 from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-def python_agent():
+def main(query: str):
     print("start...")
 
     instructions = """ You are an agent designed to write and execute python code to answer questions.
@@ -21,37 +24,52 @@ def python_agent():
     prompt = base_prompt.partial(instructions=instructions)
 
     tools = [PythonREPLTool()]
-    agent = create_react_agent(
+    python_agent = create_react_agent(
         prompt=prompt,
         llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
         tools=tools
     )
 
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-    agent_executor.invoke(
-        input={
-            "input": """generate and save in current working directory 15 QRcodes
-            that point to www.udemy.com, you have qrcode package installed already"""
-        }
-    )
+    python_agent_executor = AgentExecutor(agent=python_agent, tools=tools, verbose=True)
 
-def csv_agent():
-    print("start...")
-
-    csv_agent = create_csv_agent(
+    csv_agent_executor: AgentExecutor = create_csv_agent(
         llm=ChatOpenAI(temperature=0, model="gpt-4"),
         path="episode_info.csv",
         agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True
     )
 
-    # csv_agent.run("how many columns are there in file episode_info.csv")
-    # csv_agent.run("which writer wrote the most episodes? how many episodes did he write?")
-    # csv_agent.run("which writer wrote the least episodes? how many episodes did he write?")
-    csv_agent.run("print seasions ascending order of the number of episodes they have")
+    def python_agent_executor_wrapper(original_prompt: str) -> dict[str, Any]:
+        return python_agent_executor.invoke({"input": original_prompt})
+
+    tools = [
+        Tool(
+            name="Python Agent",
+            func=python_agent_executor_wrapper,
+            description="""useful when you need to transform natural language to python and execute the python code.
+            returning the results of the code execution
+            DOES NOT ACCEPT CODE AS INPUT"""
+        ),
+        Tool(
+            name="CSV Agent",
+            func=csv_agent_executor.invoke,
+            description="""useful when you need to answer question over episode_info.csv file,
+            takes an input the entire question and returns the answer after running pandas calculations"""
+        )
+    ]
+
+    prompt = base_prompt.partial(instructions="")
+    grand_agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+        tools=tools
+    )
+    grand_agent_executor = AgentExecutor(agent=grand_agent, tools=tools, verbose=True)
+
+    return grand_agent_executor.invoke({"input": query})
 
 
 if __name__ == '__main__':
-    # python_agent()
-    csv_agent()
+    print(main("which season has the most episodes?"))
+    print(main("generate and save in current working directory 15 qrcodes that point to 'www.udemy.com'"))
 
